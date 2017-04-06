06f876fec626ffbebfe0563afd22b29810dc7007
Zoltán Török <tzoltan.akos@gmail.com>
2017-04-06 14:12:14 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,85 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+
+using namespace std;
+
+enum Exc
+{
+    NEGATIVE_SIZE,
+    WRONG_INPUT,
+    WRONG_VALUE
+};
+
+
+template<typename T>
+istream& operator>>(istream& in, vector<T> &a)
+{
+    int size;
+    in >> size;
+    if (size < 0)
+    {
+        throw NEGATIVE_SIZE;
+    }
+    if(in.fail())
+    {
+        throw WRONG_INPUT;
+    }
+
+    for (int i = 0; i < size; ++i)
+    {
+        T temp;
+        in >> temp;
+        if (in.fail())
+        {
+            throw WRONG_INPUT;
+        }
+
+        a.push_back(temp);
+    }
+
+    return in;
+}
+
+istream& operator>>(istream& in, vector<int> &a)
+{
+    int size;
+    in >> size;
+    if(size < 0)
+    {
+        throw NEGATIVE_SIZE;
+    }
+    if(in.fail())
+    {
+        throw WRONG_INPUT;
+    }
+
+    for (int i=0; i<size; ++i)
+    {
+        int temp;
+        in >> temp;
+        if(in.fail())
+        {
+            throw WRONG_INPUT;
+        }
+        if (temp > 10 || temp < 0)
+        {
+            throw WRONG_VALUE;
+        }
+        a.push_back(temp);
+
+    }
+    return in;
+}
+
+
+
+int maxKiv(const vector<vector<int> > &data);
+int& nullaCount (vector<int> &a);
+
+#endif // HEADER_H_INCLUDED
