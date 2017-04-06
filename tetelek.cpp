d190b0e7e1948dc8c5050b638b3ed2bb9b023679
Zoltán Török <tzoltan.akos@gmail.com>
2017-04-06 14:21:42 +0200

    Update tetelek.cpp

--- tetelek.cpp
+++ tetelek.cpp
@@ -31,3 +31,34 @@ int maxKiv(const vector<vector<int> > &a)
     }
     return maxIndex;
 }
+
+istream& operator>>(istream& in, vector<int> &a)
+{
+    int size;
+    in >> size;
+    if(size < 0)
+    {
+        throw NEGATIVE_SIZE;
+    }
+    if(in.fail())
+    {
+        throw WRONG_INPUT;
+    }
+
+    for (int i=0; i<size; ++i)
+    {
+        int temp;
+        in >> temp;
+        if(in.fail())
+        {
+            throw WRONG_INPUT;
+        }
+        if (temp > 10 || temp < 0)
+        {
+            throw WRONG_VALUE;
+        }
+        a.push_back(temp);
+
+    }
+    return in;
+}
