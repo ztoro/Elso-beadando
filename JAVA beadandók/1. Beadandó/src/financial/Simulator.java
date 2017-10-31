05f164b83806d029d240d3188d1dff497c7e473f
Zoli <Zoli@DrEvil>
2017-10-31 01:26:18 +0100

    night commit

--- JAVA beadandók/1. Beadandó/src/financial/Simulator.java
+++ JAVA beadandók/1. Beadandó/src/financial/Simulator.java
@@ -1,5 +1,24 @@
 package financial;
 
-public class Simulator {
+import financial.bank.*;
+import financial.person.*;
+import java.util.*;
+import java.io.*;
 
+
+public class Simulator {
+	
+	private ATM atm;
+	private ArrayList<Customer> customers; 
+	private PrintWriter pwLog; 
+	
+	public Simulator(String bankName,int initAmount,String outputFileName) throws IOException{
+		try{
+			atm = ATM.makeATM(bankName, initAmount);
+			pwLog=new PrintWriter (outputFileName);
+			customers= new ArrayList<Customer>();
+		}
+		catch(IllegalArgumentException e){
+		}
+	}
 }
